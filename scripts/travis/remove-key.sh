7e6d5a32b1034cb79947a54d2552ccb10da77fab
ShengtaoLei <shengtao.lei@gmail.com>
2018-01-24 18:21:10 +0800

    add script

---
+++ scripts/travis/remove-key.sh
@@ -0,0 +1,3 @@
+security delete-keychain ios-build.keychain  
+rm -f ~/Library/MobileDevice/Provisioning\ Profiles/$PROFILE_NAME.mobileprovision  
+rm -f ~/Library/MobileDevice/Provisioning\ Profiles/team.mobileprovision  
---
+++ scripts/travis/sign-and-upload.sh
@@ -0,0 +1,7 @@
+PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"  
+OUTPUTDIR="$PWD/build/Release-iphoneos"  
+xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APPNAME.app" -o "$OUTPUTDIR/$APPNAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"  
+curl http://www.pgyer.com/apiv1/app/upload \  
+  -F uKey="$PGYER_UKEY" \  
+  -F file="@$OUTPUTDIR/$APPNAME.ipa" \  
+  -F _api_key="$PGYER_APIKEY"  
