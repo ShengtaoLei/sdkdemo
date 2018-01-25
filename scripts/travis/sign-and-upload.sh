9908c3de788469cafe866ff35173cd8a570a6ba8
ShengtaoLei <shengtao.lei@gmail.com>
2018-01-25 17:59:55 +0800

    Change encrypt method

--- scripts/travis/sign-and-upload.sh
+++ scripts/travis/sign-and-upload.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"  
 OUTPUTDIR="$PWD/build/Release-iphoneos"  
+
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APPNAME.app" -o "$OUTPUTDIR/$APPNAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"  
 curl http://www.pgyer.com/apiv1/app/upload \  
   -F uKey="$PGYER_UKEY" \  
