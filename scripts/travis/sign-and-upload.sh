ba17e228de5e0cca059a1c006040a4411b59932a
ShengtaoLei <shengtao.lei@gmail.com>
2018-01-30 12:06:13 +0800

    Rewrite build config.

--- scripts/travis/sign-and-upload.sh
+++ scripts/travis/sign-and-upload.sh
@@ -2,7 +2,7 @@
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"  
 OUTPUTDIR="$PWD/build/Release-iphoneos"  
 
-xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APPNAME.app" -o "$OUTPUTDIR/$APPNAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"  
+xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APPNAME.app" -o "$OUTPUTDIR/$APPNAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROFILE_NAME"  
 curl http://www.pgyer.com/apiv1/app/upload \  
   -F uKey="$PGYER_UKEY" \  
   -F file="@$OUTPUTDIR/$APPNAME.ipa" \  
