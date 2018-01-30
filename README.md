06f77efcdf9d57fb92f10eb140bd3054296f0ea0
ShengtaoLei <shengtao.lei@gmail.com>
2018-01-30 17:10:38 +0800

    remove readme

--- README.md
+++
@@ -1,8 +0,0 @@
-## PgySDK Demo
-
-[![Build Status](https://travis-ci.org/ShengtaoLei/sdkdemo.svg?branch=master)](https://travis-ci.org/ShengtaoLei/sdkdemo)
-
-This is a demo project of PGYER SDK and show how to use travis-ci.
-
-
-
--- scripts/travis/add-key.sh
+++ scripts/travis/add-key.sh
@@ -5,6 +5,7 @@ security default-keychain -d user -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
+ls -a
 security import ./scripts/travis/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign 
 security import sdk_demo.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import sdk_demo.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -A
