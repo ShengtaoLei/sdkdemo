5cb36fdf52d37adcf8ff64e7e722205b2a736b91
ShengtaoLei <shengtao.lei@gmail.com>
2018-01-31 14:12:52 +0800

    build

--- travis/scripts/sign-and-upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"  
-OUTPUTDIR="$PWD/build/Release-iphoneos"  
-
-xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APPNAME.app" -o "$OUTPUTDIR/$APPNAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROFILE_NAME"  
-curl http://www.pgyer.com/apiv1/app/upload \  
-  -F uKey="$PGYER_UKEY" \  
-  -F file="@$OUTPUTDIR/$APPNAME.ipa" \  
-  -F _api_key="$PGYER_APIKEY"  
