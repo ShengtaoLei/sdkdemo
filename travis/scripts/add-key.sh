dfec3cf50090dd024bd4fd1d985aabeb2c58329e
ShengtaoLei <shengtao.lei@gmail.com>
2018-01-31 12:45:40 +0800

    list app path

--- travis/scripts/add-key.sh
+++ travis/scripts/add-key.sh
@@ -5,7 +5,8 @@ security default-keychain -d user -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
-ls -a
+ls -a $OUTPUTDIR/$APPNAME.app
+
 security import ./travis/certificates/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign 
 security import ./travis/certificates/sdk_demo.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./travis/certificates/sdk_demo.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -A
